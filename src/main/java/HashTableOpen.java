931577dfb305eee3bb6bb10216e7e4631ab8fc08
mnl <mamontovgleb7@gmail.com>
2026-04-07 13:01:58 +0300

    start developing hash table

--- src/main/java/HashTableOpen.java
+++ src/main/java/HashTableOpen.java
@@ -1,2 +1,41 @@
 public class HashTableOpen {
+
+    private static class Node{
+        private int value;
+        private Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+
+        public Node(){
+            this.next = null;
+        }
+
+        public int getValue(){
+            return value;
+        }
+
+        public Node getNext(){
+            return this.next;
+        }
+
+        public void setValue(int value){
+            this.value = value;
+        }
+
+        public void setNext(Node next){
+            this.next = next;
+        }
+    };
+
+    private int SIZE = 10;
+    private Node[] table;
+
+    public HashTableOpen(){
+
+    }
+
+
+
 }
