affd174cef3f2360679c0a5352f5e9f3c835eeea
mnl <mamontovgleb7@gmail.com>
2026-04-07 17:30:23 +0300

    finish developing first version of the hash table

--- src/main/java/HashTableOpen.java
+++ src/main/java/HashTableOpen.java
@@ -1,41 +1,66 @@
 public class HashTableOpen {
 
     private static class Node{
-        private int value;
-        private Node next;
+        int value;
+        Node next;
 
         public Node(int value){
             this.value = value;
-        }
-
-        public Node(){
             this.next = null;
         }
 
-        public int getValue(){
-            return value;
-        }
+    }
 
-        public Node getNext(){
-            return this.next;
-        }
-
-        public void setValue(int value){
-            this.value = value;
-        }
-
-        public void setNext(Node next){
-            this.next = next;
-        }
-    };
-
-    private int SIZE = 10;
+    private int size;
     private Node[] table;
 
-    public HashTableOpen(){
+    public HashTableOpen(int size){
+        this.size = size;
+        this.table = new Node[size];
+    }
+
+    private int Hash(int num){
+        return num % size;
+    }
+
+    private Node findNodeBeforeNeeded(int num){
+        int hashedNum = Hash(num);
+        Node currentNode = this.table[hashedNum];
+        while (currentNode != null &&  currentNode.next != null && currentNode.next.value != num){
+            currentNode = currentNode.next;
+        }
+        return currentNode;
 
     }
 
+    public void Insert(int num){
+        int hashedNum = Hash(num);
+        Node newNode = new Node(num);
+        newNode.next = this.table[hashedNum];
+        this.table[hashedNum] = newNode;
+    }
 
+    public void Delete(int num){
+        int hashedNum = Hash(num);
+        if (this.table[hashedNum].value == num){
+            this.table[hashedNum] = this.table[hashedNum].next;
+            return;
+        }
+
+        Node nodeBeforeNeeded = findNodeBeforeNeeded(num);
+        if (nodeBeforeNeeded != null){
+            nodeBeforeNeeded.next = nodeBeforeNeeded.next.next;
+        }
+    }
+
+    public boolean Find(int num){
+        int hashedNum = Hash(num);
+
+        if (this.table[hashedNum].value == num){
+            return true;
+        }
+        Node nodeBeforeNeeded = findNodeBeforeNeeded(num);
+        return nodeBeforeNeeded != null;
+    }
 
 }
